12d265091d4eb5c60a994c103aa339dcf24e8a7e
Arek Chatmajian <arek.chatmajian@gmail.com>
2019-10-07 16:20:41 -0400

    added javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,139 @@
+// Select random number for NumberToGet, should be range between 19-120
+var randomNumberToGet = Math.floor(Math.random()*119+1)
+
+// Append randomNumberToGet to numberToGet id in html
+$("#numberToGet").text(randomNumberToGet);
+
+console.log(randomNumberToGet)
+
+// Select random number for each crystal, should be range between 1-12
+var number1 = Math.floor(Math.random()*12+1)
+var number2 = Math.floor(Math.random()*12+1)
+var number3 = Math.floor(Math.random()*12+1)
+var number4 = Math.floor(Math.random()*12+1)
+
+console.log(number1)
+console.log(number2)
+console.log(number3)
+console.log(number4)
+
+// Declaring starting variables for total score, wins, and losses
+var totalScore = 0;
+var wins = 0;
+var losses = 0;
+
+// Append wins and losses to corresponding html id
+$("#numberWins").text(wins);
+$("#numberLosses").text(losses);
+
+// Game reset function
+function reset() {
+    // Reset randomNumberToGet
+    randomNumberToGet = Math.floor(Math.random()*119+1)
+    $("#numberToGet").text(randomNumberToGet);
+    // Reset crystal values
+    number1 = Math.floor(Math.random()*12+1)
+    number2 = Math.floor(Math.random()*12+1)
+    number3 = Math.floor(Math.random()*12+1)
+    number4 = Math.floor(Math.random()*12+1)
+    //Reset total score variable to 0 
+    totalScore = 0;
+    $("#userScore").text(totalScore);
+}
+
+// Win 
+function win() {
+    //Create alert for win
+    alert("You win!");
+    // Add +1 to wins score
+    wins++;
+    // Append +1 to wins score text
+    $("#numberWins").text(wins);
+    // Call upon reset function
+    reset ()
+}
+
+// Lose 
+function lose() {
+    //Create alert for lose
+    alert("You lose!");
+    // Add +1 to losses score
+    losses++;
+    // Append +1 to losses score text
+    $("#numberLosses").text(losses);
+    // Call upon reset function
+    reset ()
+}
+
+// Clicks for crystals
+
+// Click function for crystal 1
+$("#img1").click (function() {
+    // Add a click of crystal value to total score
+    totalScore = totalScore + number1;
+    // Append score of crystal click to userScore id in html
+    $("#userScore").text(totalScore);
+    // Win/lose conditions
+    if (totalScore == randomNumberToGet) {
+        win ();
+    }
+    else (totalScore > randomNumberToGet) 
+        lose ();
+    }
+);
+
+// Click function for crystal 2
+$("#img2").click (function() {
+    // Add a click of crystal value to total score
+    totalScore = totalScore + number2;
+    // Append score of crystal click to userScore id in html
+    $("#userScore").text(totalScore);
+    // Win/lose conditions
+    if (totalScore == randomNumberToGet) {
+        win ();
+    }
+    else (totalScore > randomNumberToGet) 
+        lose ();
+    }
+);
+
+// Click function for crystal 3
+$("#img3").click (function() {
+    // Add a click of crystal value to total score
+    totalScore = totalScore + number3;
+    // Append score of crystal click to userScore id in html
+    $("#userScore").text(totalScore);
+    // Win/lose conditions
+    if (totalScore == randomNumberToGet) {
+        win ();
+    }
+    else (totalScore > randomNumberToGet) 
+        lose ();
+    }
+);
+
+// Click function for crystal 4
+$("#img4").click (function() {
+    // Add a click of crystal value to total score
+    totalScore = totalScore + number4;
+    // Append score of crystal click to userScore id in html
+    $("#userScore").text(totalScore);
+    // Win/lose conditions
+    if (totalScore == randomNumberToGet) {
+        win ();
+    }
+    else (totalScore > randomNumberToGet) 
+        lose ();
+    }
+);
+
+
+
+
+
 // Pseuo Coding
 
 // Set var for wins,losses, number to get, total score
-
-//Global Variables
-var numberWins = 0;
-var numberLosses = 0;
-var numberToGet = "";
-var userScore = "";
-
-console.log(numberWins);
-console.log(numberLosses);
 
 // 4 crystals, each with a unique number value
 // Set var for each crystal for each color to corresponding ID
@@ -18,32 +142,15 @@ console.log(numberLosses);
 // Crystal values must reset every new game
 // Use break to reset values
 
-// Crystal Numbers
-var crystals = [];
-var crystalsMin = 1;
-var crystalsMax = 12;
 
-
-for (
-    (i = 0; i < crystals.length ; i++)
-    
-) 
 
 
 // Generic Randomizer Function, use once for crustals values nd once for number to get
-function Random(min,max){
-    var range = (max + 1) - min;
-    var randomNumber = (Math.floor(Math.random() * range + min))
-    return randomNumber;
-}
 
 
 // Number to get value is randomly generated, with a range between 19-120
 // Use math.random -> math.floor to generate random values within range
-function Random(min,max){
-    var range = (max + 1) - min;
-    var randomNumber = (Math.floor(Math.random() * range + min))
-    return randomNumber;
+
 
 // Number to get value must reset every new game
 // Use break to reset values
@@ -58,7 +165,7 @@ function Random(min,max){
 // Wins and losses are recorded and updated after every game played
 
 
-// Recommends: put all variables in the beginning of the script page
+// Sean Recommends: put all variables in the beginning of the script page
 // Break up how the game works into a couple different functions:
 // Set up game: wins losses tracker
 // Number to get: reset to 0
